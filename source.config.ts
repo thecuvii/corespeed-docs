c3e897e580433247642809494672d882213b1983
AprilNEA <github@sku.moe>
2025-10-17 00:11:04 +0800

    refactor: refactor meta and pagetree

--- source.config.ts
+++ source.config.ts
@@ -3,13 +3,16 @@ import {
   defineDocs,
   frontmatterSchema,
   metaSchema,
-} from 'fumadocs-mdx/config';
+} from "fumadocs-mdx/config";
+import z from "zod";
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.dev/docs/mdx/collections#define-docs
 export const docs = defineDocs({
   docs: {
-    schema: frontmatterSchema,
+    schema: frontmatterSchema.extend({
+      sidebarTitle: z.string().optional(),
+    }),
   },
   meta: {
     schema: metaSchema,
